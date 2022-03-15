606da0826ea1bc58b7017f9a3717a1e39d018bd8
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-15 17:23:37 -0500

    update

---
+++ Semana1/Día1/01-intro.js
@@ -0,0 +1,44 @@
+// utilizando var, no recomendable
+var nombre = "Jorge";
+
+// let, que es identico a var pero más seguro
+//si es que van a cambiar el valor de la variable
+let apellido = "Montesinos";
+
+//const, que no permite la modificación de la variable
+//casi cualquier cosa se puede asignar a una variable
+const pi = 3.1415;
+
+// let nombre = "Osmar";
+
+// console.log(nombre);
+
+apellido = "Aramayo";
+
+console.log(apellido);
+
+// pi = 42;
+
+console.log(pi);
+
+//---------------------------------------
+
+let edad = 31;
+
+console.log(edad);
+
+edad = "treinta y uno";
+
+console.log(typeof edad);
+
+let llueve = false; //true
+
+console.log(llueve);
+
+let noDefinido; //cuando algo no existe
+
+console.log(noDefinido);
+
+let nulo = null;
+
+console.log(nulo);
