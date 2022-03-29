301244aad832a074226c824544df7757134cdf25
Osmar Montesinos <osmarmontes50@gmail.com>
2022-03-29 18:01:08 -0500

    update

--- Semana2/02-manejodom.js
+++ Semana2/02-manejodom.js
@@ -36,3 +36,26 @@ imagen.setAttribute("src", "https://picsum.photos/400");
 miBody.appendChild(imagen);
 
 miBody.style.backgroundColor = "steelblue";
+
+let botoncito = document.createElement("button");
+
+botoncito.innerHTML = "Click!!";
+
+miBody.appendChild(botoncito);
+
+botoncito.addEventListener("click", () => {
+    alert("Has dado click!");
+});
+
+let formulario = document.createElement("form");
+
+formulario.innerHTML = `
+<input type="text" name="nombre" />
+<button type="submit">Guardar</button>`;
+
+miBody.appendChild(formulario);
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    console.log(formulario.nombre.value);
+});
