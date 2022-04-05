4e93b7fd6a0dbfa462df174643d6cbfd40f7bfe7
Jorge Montesinos <osmarmontes50@gmail.com>
2022-04-05 17:45:24 -0500

    update

--- Semana4/index.js
+++ Semana4/index.js
@@ -1,49 +1,74 @@
 let pikachuOriginal = {
-    vida: 100,
-    fuerza: 80
-}
+  vida: 100,
+  fuerza: 80,
+};
 
 //REFERENCIA
 let pikachuAzul = pikachuOriginal;
 
 //SPREAD OPERATOR
-let pikachuNaranja = { ...pikachuOriginal }
-
-let pikachuVerde = { ...pikachuOriginal, toxicidad: 50, fuerza: 90 }
-
+let pikachuNaranja = { ...pikachuOriginal };
+let pikachuVerde = { ...pikachuOriginal, toxicidad: 50, fuerza: 90 };
 pikachuAzul.vida = 60;
-
 // console.log({pikachuAzul:pikachuAzul})
-console.table({ pikachuAzul })
-console.table({ pikachuOriginal })
+console.table({ pikachuAzul });
+console.table({ pikachuOriginal });
+console.table({ pikachuNaranja });
+console.table({ pikachuVerde });
 
-console.table({ pikachuNaranja })
-console.table({ pikachuVerde })
+let alumnos = ["Mijail", "Gustavo", "Jose Antonio"];
 
-let alumnos = ["Mijail","Gustavo","Jose Antonio"]
+let matriculados = [...alumnos, "Estefani"];
 
-let matriculados = [...alumnos, "Estefani"]
-
-console.table({matriculados})
+console.table({ matriculados });
 
 // DESESTRUCTURACIÓN
 
 let mascota = {
-    nombre:"Luna",
-    color:"blanca",
-    edad:4,
-}
+  nombre: "Luna",
+  color: "blanca",
+  edad: 4,
+};
 
-console.log(mascota.color)
+console.log(mascota.color);
 
-let {color, nombre} = mascota;
+let { color, nombre } = mascota;
 
-console.log(color)
+console.log(color);
 
-let frutas = ["Piña","Naranja","Uva","Lima"];
+let frutas = ["Piña", "Naranja", "Uva", "Lima"];
 
 console.log(frutas[1]);
 
 let [fruta1, fruta2, fruta3, fruta4] = frutas;
 
-console.log(fruta4)
+console.log(fruta4);
+
+//Uso en funciones
+
+const describir = ({ nombres, 
+                    edad, 
+                    hobbies = "hacer nada", 
+                    apellidos 
+                }) => {
+                    return `Esta persona se llama ${nombres} ${apellidos}, tiene ${edad} años de edad y le gusta ${hobbies}`;
+                };
+
+let persona = {
+  hobbies: "Escuchar Música",
+  nombres: "Jorge Osmar",
+  apellidos: "Montesinos Aramayo",
+  edad: 31,
+};
+
+// console.log(describir("Jorge Osmar","Montesinos Aramayo", "escuchar música", 31))
+
+console.log(describir(persona));
+
+let persona2 = {
+  nombres: "Juan",
+  apellidos: "Perez",
+  edad: 20,
+};
+
+console.log(describir(persona2));
